92b43a64034e50bbce909102bf598447b78e766e
Bhabesh Rai <brs@logpoint.com>
2020-07-30 23:50:36 +0545

    Added Sysmon support and refactored the code.

--- Sharpnel.ps1
+++ Sharpnel.ps1
@@ -4,8 +4,17 @@
 # Represents a Windows service and allows you to connect to a running or stopped service, manipulate it, or get information about it.
 # https://docs.microsoft.com/en-us/dotnet/api/system.serviceprocess.servicecontroller?view=dotnet-plat-ext-3.1
 
+Add-Type -AssemblyName System.ServiceProcess
+
 # Enumerate all the services
 [System.ServiceProcess.ServiceController]::GetServices()
 
 # Check if Windows Defender is running
-[System.ServiceProcess.ServiceController]::GetServices() | %{ if ($_.Name -eq "WinDefend" -and $_.Status -eq "Running") {echo "Windows Defender is Running"}}
+if ([System.ServiceProcess.ServiceController]::New("Sysmon").Status -eq "Running"){ echo "Windows Defender is Running"} else { echo "Windows Defender is not Running"}
+# Less efficient way
+# [System.ServiceProcess.ServiceController]::GetServices() | %{ if ($_.Name -eq "WinDefend" -and $_.Status -eq "Running") {echo "Windows Defender is Running"}}
+
+# Check if Sysmon is running and works even if Sysmon is not installed
+if ([System.ServiceProcess.ServiceController]::New("Sysmon").Status -eq "Running"){ echo "Sysmon is Running"} else{ echo "Sysmon is not Running"}
+# Less efficient way
+# [System.ServiceProcess.ServiceController]::GetServices() | %{ if ($_.Name -eq "Sysmon" -and $_.Status -eq "Running") {echo "Sysmon is Running"}}
